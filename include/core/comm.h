ffc68e52947d4b5e2ed3b50480f44210d812ce4a
sosohu <ustc.sosohu@gmail.com>
2014-11-10 20:06:36 +0800

    prepare data for test

--- include/core/comm.h
+++ include/core/comm.h
@@ -20,6 +20,8 @@ const uint32_t	NUM_BOYS = 22;
 const uint32_t	NUM_POS = 20;   // the num of pos who boy can play: ST , AMC ...
 const uint32_t	FREE_DIS = 3;	// the value declare the area of free(FREE_DIS^2)
 const uint32_t  STEAL_DIS = 3;	// the value declare the area of steal(STEAL_DIS^2)
+const uint32_t ATR_MAX_LEN = 200;
+const uint32_t NUM_ATR = 41;  // update in the time
 // Status
 const uint32_t OVER_STATUS = 0xa0;
 const uint32_t CORNER_STATUS = 0xa1;
@@ -27,5 +29,9 @@ const uint32_t FREE_STATUS = 0xa2;
 const uint32_t OUT_STATUS = 0xa3;
 const uint32_t GOAL_STATUS = 0xa4;
 const uint32_t CON_STATUS = 0xa5;
+
+//ERROR
+const uint32_t MISS_INPUT = 0xe0;
+const uint32_t OPEN_FAILED = 0xe1;
 
 #endif
--- src/People.cc
+++ src/People.cc
@@ -16,3 +16,62 @@ Tec_Atr Player::getTec_Atr(){
 Kee_Atr Player::getKee_Atr(){
 	return kee_atr;
 }
+
+void setPlay_Pos(Play_Pos pp, std::string data){
+	if(data.compare("ST") == 0){
+		pp = ST;
+		return;
+	}
+	if(data.compare("AMC") == 0){
+		pp = AMC;
+		return;
+	}
+	if(data.compare("AML") == 0){
+		pp = AML;
+		return;
+	}
+	if(data.compare("AMR") == 0){
+		pp = AMR;
+		return;
+	}
+	if(data.compare("MC") == 0){
+		pp = MC;
+		return;
+	}
+	if(data.compare("ML") == 0){
+		pp = ML;
+		return;
+	}
+	if(data.compare("MR") == 0){
+		pp = MR;
+		return;
+	}
+	if(data.compare("DM") == 0){
+		pp = DM;
+		return;
+	}
+	if(data.compare("DC") == 0){
+		pp = DC;
+		return;
+	}
+	if(data.compare("DL") == 0){
+		pp = DL;
+		return;
+	}
+	if(data.compare("DR") == 0){
+		pp = DR;
+		return;
+	}
+	if(data.compare("WL") == 0){
+		pp = WL;
+		return;
+	}
+	if(data.compare("WR") == 0){
+		pp = WR;
+		return;
+	}
+	if(data.compare("GK") == 0){
+		pp = GK;
+		return;
+	}
+}
